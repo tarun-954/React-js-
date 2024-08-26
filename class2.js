7c5d8fda87fbcc17831bd90bb36ccb9a9455a5c2
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-26 15:04:15 +0530

    class2

--- class2.js
+++ class2.js
@@ -3,3 +3,17 @@ const fun=()=>{
     console.log("My name is tarun")
 }
 fun()
+
+
+
+// arrow function with arrguments
+const func = (a,b,c) => {
+console.log(a*b*c)
+}
+func(10,20,40);
+
+// arrow function with default arguments and values predefined
+const tarun =(x,y,z=10) =>{
+console.log(x+y+z);
+}
+tarun(10,20)
