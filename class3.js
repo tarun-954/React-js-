fcb5a0e272abd165204866afddff2f8400edcb79
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-29 10:55:46 +0530

    class3

--- class3.js
+++ class3.js
@@ -1,8 +1,11 @@
+//array
 let numbers =[1,2,3,4,5]
 
+//function creation 
 function double(n){
     return n*2;
 }
+
 
 let double_value=numbers.map(double);
 // here numbers is the array 
@@ -10,3 +13,39 @@ let double_value=numbers.map(double);
 // map jod rha hai numbers array ko double function se
 
 console.log(double_value);
+
+
+let products=[
+    {name:"laptop",price:100},
+    {name:"phone",price:500},
+    {name:"tablet",price:300}
+]
+
+function increasePrice(n){
+    return {
+        ...n,
+        price:n.price*1.1
+    };
+
+}
+
+let updatedProducts=products.map(increasePrice);
+console.log(updatedProducts);
+
+
+//inbuild functions
+// creating an array with 5 elements with no values 
+let emptyArray=Array(5)
+console.log(emptyArray)
+
+//creating an array with one element and one value
+
+let filledArray=Array(1).fill("tarun")
+
+console.log(filledArray)
+
+
+
+
+
+
