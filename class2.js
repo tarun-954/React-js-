d14bb15e27c953657d6c1334cedb8ce33a2101ba
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-29 10:16:37 +0530

    class2

--- class2.js
+++ class2.js
@@ -17,3 +17,37 @@ const tarun =(x,y,z=10) =>{
 console.log(x+y+z);
 }
 tarun(10,20)
+
+
+// return object literalas
+
+// const tarun2=(author , Bname)=>{
+// ({a: author,b:Bname})
+// console.log(tarun2("TarunChoudhary","one many alone"))
+
+// }
+// tarun2();
+
+//creating a regular function
+const regularFunction={
+    name: 'Regular function',
+    getName: function()
+    {
+        console.log(this.name);
+    }
+}
+regularFunction.getName();
+
+//creating a arrow function
+
+const arrowFunction={
+    name:'arrow function',
+    getName: () =>{
+        console.log(this.name)
+    }
+}
+arrowFunction.getName();
+
+
+
+
