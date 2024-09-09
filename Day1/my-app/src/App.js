9b5d42e1a2b47a064adc488621dbae46d3344031
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-09-09 15:52:39 +0530

    day1 react

--- Day1/my-app/src/App.js
+++ Day1/my-app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+
+// import './App.css';
+
+// function App() {
+//   function newfunction(){
+
+  
+//  let arr = [1,2,3,4,5]
+//   let name ="library"
+
+// let obj={
+//   'name':"khushil",
+// 'cname':"sengari",
+// 'desc':"student",
+// }
+
+// let status =true;
+
+
+// return(
+// <div className="App">
+//   <h1>{name}</h1>
+//   {
+//     arr.map((v)=>{
+//       return (
+//         <div>{v}</div>
+//       )
+//     })
+//   }
+//    <div className="sum">{obj.name}</div>
+//   <div className="sum">{obj.cname}</div>
+//   {
+//     10+50
+//   }
+//   {(status) ? <h1 style={{color:"blueviolet"}}> "Welcome to library" </h1> : "no"}
+// </div>
+// );
+
+//   }
+
+// return (
+//   <div>{newfunction()}</div>
+
+// );
+// }
+
+// export default App;
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  let arr=["C","L","K","A"]
+let arr2=arr.sort();
+    return (
+<div>{
+arr2.map((v)=>{
+      return (
+        <div>{v}</div>
+      )
+    })
+    
 
-export default App;
+  }
+  {/* <div className="sum">{arr[2]+" "+arr[3]}</div> */}
+</div>
+    );
+  }
+  export default App;
